e481cbe12fd82f79375dc92b18d4b8fd9e0f0824
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2017-03-31 15:34:13 -0700

    made ac97 integration tb a system-level testbench (with ml505top instantiation) for checkpoint 2

--- hardware/sim/tests/ac97_integration_testbench.do
+++ hardware/sim/tests/ac97_integration_testbench.do
@@ -3,9 +3,7 @@ file copy -force ../../../software/ac97_integration_tb/ac97_integration_tb.mif i
 file copy -force ../../../software/ac97_integration_tb/ac97_integration_tb.mif dmem_blk_ram.mif
 file copy -force ../../../software/ac97_integration_tb/ac97_integration_tb.mif bios_mem.mif
 add wave ac97_integration_testbench/*
-add wave ac97_integration_testbench/ac97_fifo/*
-add wave ac97_integration_testbench/ac97_fifo/buff/*
-add wave ac97_integration_testbench/audio_controller/*
+add wave ac97_integration_testbench/top/*
 add wave ac97_integration_testbench/codec_model/*
 add wave ac97_integration_testbench/codec_model/codec_ready
 add wave ac97_integration_testbench/codec_model/bit_counter
--- hardware/src/testbenches/ac97_integration_testbench.v
+++ hardware/src/testbenches/ac97_integration_testbench.v
@@ -7,8 +7,8 @@
 // The program transmits the PCM samples -50, -49, -48, ..., 0, 1, 2, ..., 48, 49, 50.
 // You should inspect the waveform and verify that the AC97 codec model receives each sample in order by inspecting Slot 3 and Slot 4.
 module ac97_integration_testbench();
-    parameter SYSTEM_CLK_PERIOD = 20;
-    parameter SYSTEM_CLK_FREQ = 50_000_000;    
+    parameter SYSTEM_CLK_PERIOD = 10;
+    parameter SYSTEM_CLK_FREQ = 100_000_000;    
 
     reg sys_clk = 0;
     reg sys_rst = 0;
@@ -16,58 +16,47 @@ module ac97_integration_testbench();
     // AC97 Signals
     wire bit_clk, sdata_out, sdata_in, reset_b, sync;
 
-    // AC97 Sample FIFO Signals
-    wire ac97_fifo_wr_en, ac97_fifo_rd_en, ac97_fifo_empty, ac97_fifo_full;
-    wire [19:0] ac97_fifo_din, ac97_fifo_dout;
-
     always #(SYSTEM_CLK_PERIOD/2) sys_clk <= ~sys_clk;
 
-    // In this testbench, we instantiate the RISCV CPU, an async FIFO, the AC97 controller, and the AC97 codec model
+    // In this testbench, we instantiate ml505top which should contain your RISCV CPU, an async FIFO, and the AC97 controller
+    // We also instantiate the ac97_codec_model in the testbench to check that the expected AC97 frames are transmitted.
+        
+    ml505top # (
+        .SYSTEM_CLOCK_FREQ(SYSTEM_CLK_FREQ),
+        .B_SAMPLE_COUNT_MAX(1),
+        .B_PULSE_COUNT_MAX(1),
+        .R_SAMPLE_COUNT_MAX(1),
+        .R_PULSE_COUNT_MAX(1)
+    ) top (
+        .USER_CLK(sys_clk),
 
-    // You may have to change the port list on your CPU depending on whether you instantiated the AC97 controller/FIFO
-    // in the CPU itself or in ml505top.
-    Riscv151 #(
-        .CPU_CLOCK_FREQ(SYSTEM_CLK_FREQ)
-    ) CPU(
-        .clk(sys_clk),
-        .rst(sys_rst),
-        .AC97_val(ac97_fifo_wr_en),
-        .AC97_data(ac97_fifo_din),
-        .AC97_full(ac97_fifo_full)
+        .GPIO_DIP(8'd0),
+        .FPGA_ROTARY_INCA(1'b0),
+        .FPGA_ROTARY_INCB(1'b0),
+        .FPGA_ROTARY_PUSH(1'b0),
+        .GPIO_BUTTONS(5'd0),
+        .FPGA_CPU_RESET_B(~sys_rst),
+
+        .PIEZO_SPEAKER(),
+        .GPIO_LED(),
+        .GPIO_LED_C(),
+        .GPIO_LED_N(),
+        .GPIO_LED_E(), 
+        .GPIO_LED_W(),
+        .GPIO_LED_S(),
+
+        // AC97 Protocol Signals
+        .AUDIO_BIT_CLK(bit_clk),
+        .AUDIO_SDATA_IN(sdata_in),
+        .AUDIO_SDATA_OUT(sdata_out),
+        .AUDIO_SYNC(sync),
+        .FLASH_AUDIO_RESET_B(reset_b),
+
+        // UART connections
+        .FPGA_SERIAL_RX(1'b1),
+        .FPGA_SERIAL_TX()
     );
 
-    // Your async FIFO with the write interface exposed to the CPU and read interface exposed to the AC97 controller
-    async_fifo #(
-        .data_width(20),
-        .fifo_depth(8)
-    ) ac97_fifo (
-        .wr_clk(sys_clk),
-        .rd_clk(bit_clk),
-        .wr_en(ac97_fifo_wr_en),
-        .rd_en(ac97_fifo_rd_en),
-        .din(ac97_fifo_din),
-        .full(ac97_fifo_full),
-        .empty(ac97_fifo_empty),
-        .dout(ac97_fifo_dout)
-    );
-
-    // The AC97 controller is hooked to the read side of the async FIFO 
-    ac97_controller #(
-        .SYS_CLK_FREQ(SYSTEM_CLK_FREQ)
-    ) audio_controller (
-        .sdata_in(sdata_in),
-        .sdata_out(sdata_out),
-        .bit_clk(bit_clk),
-        .sample_fifo_tone_data(ac97_fifo_dout),
-        .sample_fifo_empty(ac97_fifo_empty),
-        .sample_fifo_rd_en(ac97_fifo_rd_en),
-        .sync(sync),
-        .reset_b(reset_b),
-        .volume_control(4'b0),  // Change this to connect to the memory mapped register coming from your CPU
-        .system_clock(sys_clk),
-        .system_reset(sys_rst)
-    );
-   
     ac97_codec_model codec_model (
         .sdata_out(sdata_out),
         .bit_clk(bit_clk),
@@ -88,7 +77,7 @@ module ac97_integration_testbench();
         // Wait for a few more frames to see that PCM sample 50 is being transmitted even after the FIFO is empty
         repeat(256 * 10) @(posedge bit_clk);
 
-        // Inspect the waveform to see that all the PCM samples were sent and that the FIFO is empty!!
+        // Inspect the waveform to see that all the PCM samples were sent and that the async FIFO is empty!!
         $finish();
     end
 
